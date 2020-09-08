37f391549ada2cd59431bf4bd51ed5eafb7fa960
wyao <289672494@qq.com>
2020-09-08 13:49:19 +0800

    GenericTest finish

--- src/test/java/com/twc/javaBasic/GenericTest.java
+++ src/test/java/com/twc/javaBasic/GenericTest.java
@@ -14,7 +14,7 @@ class GenericTest {
     //  The getMiddle method is a generic method. Now, please call getMiddle method for string
     //  type.
     // <--start
-    final String middle = null;
+    final String middle = getMiddle(words);
     // --end-->
 
     assertEquals("Good", middle);
@@ -52,8 +52,13 @@ class GenericTest {
   //  You should not change the signature of the function. But you can change
   //  the declaration of the generic type parameter.
   // <--start
-  private static <T> T min(T[] values) {
-    throw new RuntimeException("Not implemented");
+  private static <T extends Comparable> T min(T[] values) {
+    T res = values[0];
+    for (int i = 0; i < values.length; i++) {
+      res = res.compareTo(values[i]) > 0 ? values[i] : res;
+    }
+    return res;
+    //throw new RuntimeException("Not implemented");
   }
   // --end-->
 
@@ -64,8 +69,11 @@ class GenericTest {
   // Hint:
   //  A wildcard is not a type variable, so we can’t write code that uses ? as a type.
   // <--start
-  private static void swap(Pair<?> pair) {
-    throw new RuntimeException("Not implemented");
+  private static <T> void swap(Pair<T> pair) {
+    T a = pair.getFirst();
+    pair.setFirst(pair.getSecond());
+    pair.setSecond(a);
+    //throw new RuntimeException("Not implemented");
   }
 
   // TODO: You can add additional method within the range if you like
